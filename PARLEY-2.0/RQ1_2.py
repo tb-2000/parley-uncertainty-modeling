890691e34c0044f6782dc6f9e01260a80362d1f7
tb-2000 <tomblum@gmx.de>
2026-08-17 16:40:38 +0200

    prepare for run on maps 83,85,87

--- PARLEY-2.0/RQ1_2.py
+++ PARLEY-2.0/RQ1_2.py
@@ -61,8 +61,8 @@ def main():
      54, 55, 56, 57, 63, 66, 71, 75, 81, 82, 83, 85, 87, 89, 90, 97]
                     
     # maps()
-    maps = [75, 81, 82] # selected_maps
-    for i in maps: # lasse auf map 75,81,82 laufen
+    maps = [83, 85, 87] # selected_maps
+    for i in maps: # lasse auf maps 83,85,87 laufen
         models(i)
         #baseline(i)
         print('Starting EvoChecker for map {0}'.format(str(i)))
