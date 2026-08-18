fc68fa9471d5bbb89a0e1554e20d17f3df2cef94
tb-2000 <tomblum@gmx.de>
2026-08-18 15:02:57 +0200

    prepare run on map 30 again

--- PARLEY-2.0/test_umc.py
+++ PARLEY-2.0/test_umc.py
@@ -13,7 +13,7 @@ def main():
     #     urc_synthesis.manipulate_prism_model(infile, outfile, baseline=False)
     #     print(f"generated: {outfile}")
 
-    i=46
+    i=32
     infile = f'Applications/EvoChecker-master/models/model_{i}.prism'
     outfile = f'Applications/EvoChecker-master/models/model_{i}_umc.prism'
     prism_model_generator_belief_full.generate_model(i)
