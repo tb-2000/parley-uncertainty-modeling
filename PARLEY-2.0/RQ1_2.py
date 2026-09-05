4705456f9ca7a561bd7cc70adae594c7f1a8fc55
tb-2000 <tomblum@gmx.de>
2026-09-05 12:29:40 +0200

    prepare run on maps 90, 97 belief

--- PARLEY-2.0/RQ1_2.py
+++ PARLEY-2.0/RQ1_2.py
@@ -60,8 +60,8 @@ def main():
                   54, 55, 56, 57, 63, 66, 71, 75, 81, 82, 83, 85, 87, 89, 90, 97]
                     
     # maps()
-    maps = [87, 89] # selected_maps
-    for i in maps: # lasse auf map 81, 82 laufen
+    maps = [90, 97] # selected_maps
+    for i in maps: # lasse auf map 90, 97 laufen
         models(i)
         #baseline(i)
         print('Starting EvoChecker for map {0}'.format(str(i)))
--- PARLEY-2.0/test_umc.py
+++ PARLEY-2.0/test_umc.py
@@ -17,7 +17,7 @@ def main():
     #     urc_synthesis.manipulate_prism_model(infile, outfile, baseline=False)
     #     print(f"generated: {outfile}")
 
-    maps = [87, 89] # selected_maps
+    maps = [90, 97] # selected_maps
     for i in maps:
         infile = f'Applications/EvoChecker-master/models/model_{i}.prism'
         outfile = f'Applications/EvoChecker-master/models/model_{i}_umc.prism'
