201bd12cd54e8842a5b95c03f2e0d6b9754818f9
tb-2000 <tomblum@gmx.de>
2026-09-09 12:26:52 +0200

    run evochecker seriell angepasst

--- PARLEY-2.0/run_evochecker.py
+++ PARLEY-2.0/run_evochecker.py
@@ -1,32 +1,44 @@
 import os
 import time
-from multiprocessing import Pool, cpu_count
+import subprocess
+from pathlib import Path
 
 
 def run_task(args):
-    os.chdir('Applications/EvoChecker-master')
-    os.environ['LD_LIBRARY_PATH'] = "libs/runtime"
     i, rep = args
-    path = "./{0}_{1}.properties".format(str(i), str(rep))
-    open(path, "w").close()
+
+    # Do not change the working directory of the Python process.
+    # Otherwise the second serial replication would try to chdir into
+    # Applications/EvoChecker-master a second time.
+    evochecker_dir = Path(__file__).resolve().parent / "Applications" / "EvoChecker-master"
+    properties_name = f"{i}_{rep}.properties"
+    properties_path = evochecker_dir / properties_name
 
     init_port = 10000 + i * 100 + rep * 10
 
-    with open(path, 'a') as f:
-        f.write("PROBLEM = ROBOT{0}_REP{1}\n".format(str(i), str(rep)))
-        f.write("       MODEL_TEMPLATE_FILE = models/model_{0}_umc.prism\n".format(str(i)))
+    with open(properties_path, "w") as f:
+        f.write(f"PROBLEM = ROBOT{i}_REP{rep}\n")
+        f.write(f"       MODEL_TEMPLATE_FILE = models/model_{i}_umc.prism\n")
         f.write("       PROPERTIES_FILE = robot.pctl\n")
         f.write("       ALGORITHM = NSGAII\n")
-        f.write("       POPULATION_SIZE = 100\n") # 100
-        f.write("       MAX_EVALUATIONS = 4000\n") # 4000
-        f.write("       PROCESSORS = 6\n") # 6 PrismExecutor workers per replication
+        f.write("       POPULATION_SIZE = 100\n")
+        f.write("       MAX_EVALUATIONS = 4000\n")
+        f.write("       PROCESSORS = 6\n")
         f.write("       PLOT_PARETO_FRONT = false\n")
         f.write("       VERBOSE = true\n")
-        #f.write("       INIT_PORT = 55{0}\n".format(str(i)))
-        f.write("       INIT_PORT = {init_port}\n".format(init_port=init_port))
-    # Note: INIT_PORT doesn't have an effect https://github.com/gerasimou/EvoChecker/issues/11
+        f.write(f"       INIT_PORT = {init_port}\n")
 
-    os.system('java -jar ./target/EvoChecker-1.1.0.jar ' + path)
+    env = os.environ.copy()
+    env["LD_LIBRARY_PATH"] = "libs/runtime"
+
+    # This blocks until the current EvoChecker replication has completely
+    # finished. Then run() starts the next replication.
+    subprocess.run(
+        ["java", "-jar", "./target/EvoChecker-1.1.0.jar", f"./{properties_name}"],
+        cwd=evochecker_dir,
+        env=env,
+        check=True,
+    )
 
 
 def run(map_, replications):
