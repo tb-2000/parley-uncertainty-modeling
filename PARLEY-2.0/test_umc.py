85a38171bfcd691155f50cd637535f7646c373d6
tb-2000 <tomblum@gmx.de>
2026-09-13 23:53:06 +0200

    prepare run on maps 75,81,82 gaussian

--- PARLEY-2.0/test_umc.py
+++ PARLEY-2.0/test_umc.py
@@ -13,7 +13,7 @@ def main():
     #     urc_synthesis.manipulate_prism_model(infile, outfile, baseline=False)
     #     print(f"generated: {outfile}")
 
-    maps = [66, 71] # selected_maps
+    maps = [75, 81, 82] # selected_maps
     for i in maps:
         infile = f'Applications/EvoChecker-master/models/model_{i}.prism'
         outfile = f'Applications/EvoChecker-master/models/model_{i}_umc.prism'
