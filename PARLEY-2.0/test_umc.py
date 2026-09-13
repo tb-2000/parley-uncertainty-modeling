4a51586ce680a0a864f2e87c5cf6cd1efe4561e9
tb-2000 <tomblum@gmx.de>
2026-09-13 21:21:30 +0200

    prepare run on maps 23, 30 hmm

--- PARLEY-2.0/test_umc.py
+++ PARLEY-2.0/test_umc.py
@@ -2,7 +2,7 @@ import urc_synthesis_hmm_exact_local
 import prism_model_generator_hmm_exact_local
 
 def main():
-    maps = [14, 21] # selected_maps
+    maps = [23, 30] # selected_maps
     for i in maps:
         infile = f'Applications/EvoChecker-master/models/model_{i}.prism'
         outfile = f'Applications/EvoChecker-master/models/model_{i}_umc.prism'
