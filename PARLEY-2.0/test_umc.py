b1b4e0ca8a05e3d803f8802c19dadb842c3530db
tb-2000 <tomblum@gmx.de>
2026-09-16 00:14:07 +0200

    lauf für maps 48, 49 belief

--- PARLEY-2.0/test_umc.py
+++ PARLEY-2.0/test_umc.py
@@ -17,7 +17,7 @@ def main():
     #     urc_synthesis.manipulate_prism_model(infile, outfile, baseline=False)
     #     print(f"generated: {outfile}")
 
-    maps = [46, 47] # selected_maps
+    maps = [48, 49] # selected_maps
     for i in maps:
         infile = f'Applications/EvoChecker-master/models/model_{i}.prism'
         outfile = f'Applications/EvoChecker-master/models/model_{i}_umc.prism'
